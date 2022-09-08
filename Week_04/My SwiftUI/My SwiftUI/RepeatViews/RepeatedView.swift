55d3af482b3dac617ad3665c8f4455c90b610652
J Rogel <jrogel.datascience@icloud.com>
2022-09-08 11:33:05 +0100

    Challenge - Navigation

--- Week_04/My SwiftUI/My SwiftUI/RepeatViews/RepeatedView.swift
+++ Week_04/My SwiftUI/My SwiftUI/RepeatViews/RepeatedView.swift
@@ -9,25 +9,38 @@ import SwiftUI
 
 struct RepeatedView: View {
     let mix = MeowMix()
+    @State private var showMessage = false
     
     var body: some View {
-        VStack(spacing: 0) {
-            MeowMixHeader()
-                .padding()
-            
-            Divider()
-                .padding()
-            
-            // Add a List of tracks
-            List(mix.tracks) { track in
-                TrackRow(track: track)
+        NavigationView {
+            VStack(spacing: 0) {
+                MeowMixHeader()
+                    .padding()
+                    .onTapGesture {
+                        showMessage = true
+                    }
+                    .popover(isPresented: $showMessage, content: {
+                        MessagePopover()
+                    })
+                
+                Divider()
+                    .padding()
+                
+                // Add a List of tracks
+                List(mix.tracks) { track in
+                    NavigationLink(destination: DetailView(track: track), label: {
+                        TrackRow(track: track)
+                    })
+                }
+                
+                // Add FeaturedCats view
+                FeaturedCats(artists: mix.tracks.map(\.artist))
+                    .padding(.vertical)
+                    .backgroundStyle(Color.gray.opacity(0.2))
             }
-            
-            // Add FeaturedCats view
-            FeaturedCats(artists: mix.tracks.map(\.artist))
-                .padding(.vertical)
-                .backgroundStyle(Color.gray.opacity(0.2))
+            .navigationBarHidden(true)
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
@@ -115,5 +128,40 @@ struct FeaturedArtist: View {
         }
         .frame(width: 120)
     }
+}
+
+
+struct DetailView: View {
+  let track: Track
+  
+  var body: some View {
+    ZStack {
+      track.gradient
+        .ignoresSafeArea()
+      
+      VStack {
+        Text(track.title)
+          .font(.largeTitle)
+          .fontWeight(.black)
+        Text(track.artist)
+          .font(.title)
+          .fontWeight(.semibold)
+        
+        track.thumbnail
+          .resizable()
+          .scaledToFit()
+          .padding(50)
+      }
+      .foregroundColor(.white)
+    }
+  }
+}
+
+struct MessagePopover: View {
+  var body: some View {
+    Text("These cats are not available in your region 😿")
+      .font(.title)
+      .padding()
+  }
 }
 
