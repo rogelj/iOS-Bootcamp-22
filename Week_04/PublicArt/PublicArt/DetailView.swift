77a589807428860b964f1f714549a6107ad86d1d
J Rogel <jrogel.datascience@icloud.com>
2022-09-08 11:21:58 +0100

    Present moda views

--- Week_04/PublicArt/PublicArt/DetailView.swift
+++ Week_04/PublicArt/PublicArt/DetailView.swift
@@ -33,46 +33,50 @@
 import SwiftUI
 
 struct DetailView: View {
-  let artwork: Artwork
-  
-  var body: some View {
-    VStack {
-      Text(artwork.title)
-        .font(.title2)
-        .multilineTextAlignment(.center)
-        .lineLimit(3)
-      
-      Button(action: {  }) {
-        HStack(alignment: .firstTextBaseline) {
-          Image(systemName: "mappin.and.ellipse")
-          
-          Text(artwork.locationName)
-            .font(.headline)
+    let artwork: Artwork
+    @State private var showMap = false
+    
+    var body: some View {
+        VStack {
+            Text(artwork.title)
+                .font(.title2)
+                .multilineTextAlignment(.center)
+                .lineLimit(3)
+            
+            Button(action: { showMap = true }) {
+                HStack(alignment: .firstTextBaseline) {
+                    Image(systemName: "mappin.and.ellipse")
+                    
+                    Text(artwork.locationName)
+                        .font(.headline)
+                }
+            }
+            .fullScreenCover(isPresented: $showMap) {
+                LocationMap(artwork: artwork, showModal: $showMap)
+            }
+            
+            Text("Artist: \(artwork.artist)")
+                .font(.subheadline)
+            
+            Image(artwork.imageName)
+                .resizable()
+                .frame(maxWidth: 300, maxHeight: 600)
+                .aspectRatio(contentMode: .fit)
+            
+            Divider()
+            
+            Text(artwork.description)
+                .multilineTextAlignment(.leading)
+                .lineLimit(20)
         }
-      }
-      
-      Text("Artist: \(artwork.artist)")
-        .font(.subheadline)
-      
-      Image(artwork.imageName)
-        .resizable()
-        .frame(maxWidth: 300, maxHeight: 600)
-        .aspectRatio(contentMode: .fit)
-      
-      Divider()
-      
-      Text(artwork.description)
-        .multilineTextAlignment(.leading)
-        .lineLimit(20)
+        .padding()
+        .navigationBarTitleDisplayMode(.inline)
     }
-    .padding()
-    .navigationBarTitleDisplayMode(.inline)
-  }
 }
 
 struct DetailView_Previews: PreviewProvider {
-  static var previews: some View {
-    DetailView(artwork: artData[0])
-  }
+    static var previews: some View {
+        DetailView(artwork: artData[0])
+    }
 }
 
--- Week_04/PublicArt/PublicArt/LocationMap.swift
+++ Week_04/PublicArt/PublicArt/LocationMap.swift
@@ -34,29 +34,32 @@ import SwiftUI
 import MapKit
 
 struct LocationMap: View {
-  var artwork: Artwork
-  @State var region = MKCoordinateRegion()
-
-  var body: some View {
-    VStack {
-      HStack {
-        Text(self.artwork.locationName)
-      }
-      .padding()
-      
-      Map(coordinateRegion: $region, annotationItems: [artwork]) { artwork in
-        MapMarker(coordinate: artwork.coordinate)
-      }
-      .ignoresSafeArea()
-      .onAppear {
-        region.center = artwork.coordinate
-        region.span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02) }
+    var artwork: Artwork
+    @State var region = MKCoordinateRegion()
+    @Binding var showModal: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(self.artwork.locationName)
+                Spacer()
+                Button("Done") { showModal = false }
+            }
+            .padding()
+            
+            Map(coordinateRegion: $region, annotationItems: [artwork]) { artwork in
+                MapMarker(coordinate: artwork.coordinate)
+            }
+            .ignoresSafeArea()
+            .onAppear {
+                region.center = artwork.coordinate
+                region.span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02) }
+        }
     }
-  }
 }
 
 struct LocationMap_Previews: PreviewProvider {
-  static var previews: some View {
-    LocationMap(artwork: artData[0])
-  }
+    static var previews: some View {
+        LocationMap(artwork: artData[0], showModal: .constant(true))
+    }
 }
