8991926499c9bd2c7bb0973ba260921ffecf103a
J Rogel <jrogel.datascience@icloud.com>
2022-09-13 11:56:58 +0100

    Geometry Reader

---
+++ Week_05/Layout/Layout/GeometryReaderView.swift
@@ -0,0 +1,27 @@
+
+
+import SwiftUI
+
+struct GeometryReaderView: View {
+    var body: some View {
+        GeometryReader { proxy in
+            HStack {
+                Image("Cake VStack")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: proxy.size.width * 0.5)
+                Text("Reading is dreaming with open eyes.")
+                    .layoutPriority(1)
+                Image("Pancake VStack")
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+    }
+}
+
+struct GeometryReaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryReaderView()
+    }
+}
