8f193434815d1e36da298422665cd0d8c64e8291
J Rogel <jrogel.datascience@icloud.com>
2022-09-19 14:42:28 +0100

    Use Delegation

--- Week_06/RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
+++ Week_06/RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
@@ -28,23 +28,56 @@
 
 import UIKit
 
+public protocol QuestionViewControllerDelegate: class {
+  func questionViewController(_ viewController: QuestionViewController,
+                              didCancel questionGroup: QuestionGroup,
+                              at questionIndex: Int)
+  
+  func questionViewController(_ viewController: QuestionViewController,
+                              didComplete questionGroup: QuestionGroup)
+}
+
 public class QuestionViewController: UIViewController {
 
   // MARK: - Instance Properties
-  public var questionGroup = QuestionGroup.basicPhrases()
+  public weak var delegate: QuestionViewControllerDelegate?
+  
+  public var questionGroup = QuestionGroup.basicPhrases() {
+    didSet {
+      navigationItem.title = questionGroup.title
+    }
+  }
   public var questionIndex = 0
   
   public var correctCount = 0
   public var incorrectCount = 0
+  
   public var questionView: QuestionView! {
     guard isViewLoaded else { return nil }
     return (view as! QuestionView)
   }
   
+  private lazy var questionIndexItem: UIBarButtonItem = { [unowned self] in
+    let item = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    self.navigationItem.rightBarButtonItem = item
+    return item
+  }()
+  
   // MARK: - View Lifecycle
   public override func viewDidLoad() {
     super.viewDidLoad()
     showQuestion()
+    setupCancelButton()
+  }
+  
+  private func setupCancelButton() {
+    let action = #selector(handleCancelPressed(sender: ))
+    let image = UIImage(named: "ic_menu")
+    navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, landscapeImagePhone: nil, style: .plain, target: self, action: action)
+  }
+  
+  @objc private func handleCancelPressed(sender: UIBarButtonItem) {
+    delegate?.questionViewController(self, didCancel: questionGroup, at: questionIndex)
   }
   
   private func showQuestion() {
@@ -56,6 +89,8 @@ public class QuestionViewController: UIViewController {
     
     questionView.answerLabel.isHidden = true
     questionView.hintLabel.isHidden = true
+    
+    questionIndexItem.title = "\(questionIndex + 1)/\(questionGroup.questions.count)"
   }
   
   // MARK: - Actions
@@ -79,7 +114,7 @@ public class QuestionViewController: UIViewController {
   private func showNextQuestion() {
     questionIndex += 1
     guard questionIndex < questionGroup.questions.count else {
-      // TODO: Handle this
+      delegate?.questionViewController(self, didComplete: questionGroup)
       return
     }
     showQuestion()
