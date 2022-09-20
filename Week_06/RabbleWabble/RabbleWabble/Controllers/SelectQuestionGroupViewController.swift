5f4fb46b2e39151913719f9a6c988b3b276cb9ad
J Rogel <jrogel.datascience@icloud.com>
2022-09-20 20:12:04 +0100

    Use builder

--- Week_06/RabbleWabble/RabbleWabble/Controllers/SelectQuestionGroupViewController.swift
+++ Week_06/RabbleWabble/RabbleWabble/Controllers/SelectQuestionGroupViewController.swift
@@ -98,9 +98,17 @@ extension SelectQuestionGroupViewController: UITableViewDelegate {
   }
   
   public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    guard let viewController = segue.destination as? QuestionViewController else { return }
-    viewController.questionStrategy = appSettings.questionStrategy(for: questionGroupCaretaker)
-    viewController.delegate = self
+//    guard let viewController = segue.destination as? QuestionViewController else { return }
+//    viewController.questionStrategy = appSettings.questionStrategy(for: questionGroupCaretaker)
+//    viewController.delegate = self
+    if let viewController = segue.destination as? QuestionViewController {
+      viewController.questionStrategy = appSettings.questionStrategy(for: questionGroupCaretaker)
+      viewController.delegate = self
+    } else if let navController = segue.destination as? UINavigationController,
+              let viewController = navController.topViewController as? CreateQuestionGroupViewController {
+      viewController.delegate = self
+    }
+    // whatevs... skip anything else
   }
 }
 
@@ -114,4 +122,20 @@ extension SelectQuestionGroupViewController: QuestionViewControllerDelegate {
                                      didComplete questionStrategy: QuestionStrategy) {
     navigationController?.popToViewController(self, animated: true)
   }
+}
+
+extension SelectQuestionGroupViewController: CreateQuestionGroupViewControllerDelegate {
+  
+  public func createQuestionGroupViewControllerDidCancel(_ viewController: CreateQuestionGroupViewController) {
+    dismiss(animated: true, completion: nil)
+  }
+  
+  public func createQuestionGroupViewController(_ viewController: CreateQuestionGroupViewController, created questionGroup: QuestionGroup) {
+    questionGroupCaretaker.questionGroups.append(questionGroup)
+    try? questionGroupCaretaker.save()
+    dismiss(animated: true, completion: nil)
+    tableView.reloadData()
+  }
+  
+  
 }
