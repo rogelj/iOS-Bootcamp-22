90b3f870f312dde665506958d4acf715c9046423
J Rogel <jrogel.datascience@icloud.com>
2022-09-20 16:37:03 +0100

    Observer

--- Week_06/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Observer.xcplaygroundpage/Contents.swift
+++ Week_06/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Observer.xcplaygroundpage/Contents.swift
@@ -11,3 +11,26 @@
  
  ## Code Example
  */
+
+import Combine
+
+public class User {
+    
+    @Published var name: String
+    
+    public init(name: String) {
+        self.name = name
+    }
+}
+
+let user = User(name: "Ray")
+let publisher = user.$name
+
+var subscriber: AnyCancellable? = publisher.sink() {
+    print("User's name is \($0)")
+}
+
+user.name = "Vicki"
+
+subscriber = nil
+user.name = "Ray has left the building"
