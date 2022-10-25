790d32f469694d3264c1bbe5fd6c74686e3b1d3e
J Rogel <jrogel.datascience@icloud.com>
2022-10-25 16:09:47 +0100

    Caching images

--- Week_11/05_EmojiArt/EmojiArt/Views/DetailsView.swift
+++ Week_11/05_EmojiArt/EmojiArt/Views/DetailsView.swift
@@ -35,7 +35,7 @@ import SwiftUI
 struct DetailsView: View {
   let file: ImageFile
   @State var image: UIImage?
-  @EnvironmentObject var imageLoader: ImageLoader
+//  @EnvironmentObject var imageLoader: ImageLoader
 
   var body: some View {
     ZStack(alignment: .bottom) {
@@ -64,7 +64,7 @@ struct DetailsView: View {
     .ignoresSafeArea()
     .foregroundColor(.white)
     .task {
-      image = try? await imageLoader.image(file.url)
+      image = try? await ImageDatabase.shared.image(file.url)
     }
   }
 }
